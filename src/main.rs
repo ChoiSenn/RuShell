13f2315eec50f3ec9f71836ee28b898d558f120a
ChoiSenn <quitechoisen@gmail.com>
2026-02-16 15:40:30 +0900

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,14 @@
 use std::io::{self, Write};
 
 fn main() {
+    // 프롬프트 시작
     print!("$ ");
+    io::stdout().flush().unwrap();
 
+    // 입력 값 읽어와 저장 (command)
     let mut command = String::new();
     io::stdin().read_line(&mut command).unwrap();
-    
-    //io::stdout().flush().unwrap();
+
+    // 처리 및 출력
     println!("{}: command not found", command.trim());
 }
