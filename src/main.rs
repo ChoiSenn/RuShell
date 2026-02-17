57a652f7ecbaa3d35d2d65bcd0f62f659837e49f
ChoiSenn <quitechoisen@gmail.com>
2026-02-17 21:04:05 +0900

    progress

--- src/main.rs
+++ src/main.rs
@@ -10,15 +10,28 @@ fn main() {
         io::stdout().flush().unwrap();
 
         // 입력 값 읽어와 저장 (command)
-        let mut command = String::new();
-        io::stdin().read_line(&mut command).unwrap();
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        let input = input.trim();
 
-        let command = command.trim();
+        if input.is_empty() {
+            continue;
+        }
+
+        // 입력 값을 공백 기준으로 분해하여 &str 슬라이스 벡터 생성
+        let mut input_command = input.split_whitespace();
+        let command = input_command.next().unwrap();
+        let args: Vec<&str> = input_command.collect();
 
         // exit 입력 시, 종료
         match command {
             "exit" => break,
+            "echo" => echo_command(&args),
             _ => println!("{}: command not found", command.trim()),
         }
     }
+}
+
+fn echo_command(args: &[&str]) {
+    println!("{}", args.join(" "));
 }
